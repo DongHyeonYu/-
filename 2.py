bdb7e72eaac0d1d790028bff81fe2d9db51952b6
DongHyeonYu <66219597+DongHyeonYu@users.noreply.github.com>
2024-08-30 15:17:06 +0900

    Refactoring 2.py
    Refactor code to modularize functions for improved readability and reusability

--- 2.py
+++ 2.py
@@ -1,14 +1,30 @@
-a = list(map(int,input().split()))
-b = list(map(int,input().split()))
 
-print(list(set(a)))
+def duplicate(a):
+    return list(set(a))
 
-print(list(set(a)) + list(set(b)))
+def merge(a,b):
+    return list(set(a+b))
 
-c = list()
-for i in a:
-    for j in b:
-        if i == j:
-            c.append(i)
+def intersection(a,b):
+    c = list()
+    for i in a:
+        for j in b:
+            if i == j:
+                c.append(i)
+    return c
+
+
             
-print(list(set(c)))
+if __name__ =='__main__':
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    print(a)
+    print(b)
+    x = duplicate(a)
+    y = merge(a,b)
+    z = intersection(a,b)
+    print(x)
+    print(y)
+    print(z)
+
+
