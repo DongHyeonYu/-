f38dd586716f8dd2303fca77ba4646a58305cadb
DongHyeonYu <66219597+DongHyeonYu@users.noreply.github.com>
2024-08-30 15:02:08 +0900

    Refactor: Simplified code_1.py

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
 a = input()
-if a.startswith('-'):
-    b = list(a)[1:len(a)]
+p = print
+if a[0]=='-':
+    b = list(a)[1:]
 else:
     b = list(a)
 cnt =0
@@ -8,7 +9,7 @@ for i in range(1,len(b)):
     if i%3==0:
         b.insert(-(i+cnt),',')
         cnt+=1
-if a.startswith('-'):
-    print(f"-{''.join(b)}")
+if a[0]=='-':
+    p(f"-{''.join(b)}")
 else:
-    print(''.join(b))
+    p(''.join(b))
